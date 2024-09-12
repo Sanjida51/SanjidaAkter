32742444c59802e09462636373a0dd9115ceb4bd
Sanjida <sanjidaakter.iiuc.ctg.bd@gmail.com>
2024-09-13 00:13:18 +0600

    added all files

---
+++ script.js
@@ -0,0 +1,24 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Document loaded');
+
+    // Smooth scrolling for navigation links //Sanjida Akter
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+// Sanjida//Sanjida Akter
